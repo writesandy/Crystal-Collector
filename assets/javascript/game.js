e589e5e6abca7d6f122a339e7c24cf330dd838ed
writesandy <writesandy@gmail.com>
2018-03-10 10:03:45 -0600

    updates to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,60 +3,72 @@ $(document).ready(function(){
 // Variables
 
 let score = "";
-let guessValue = "";
-let crystalVals = [];
-let wins = "";
-let losses = "";
+let wins = 0;
+let losses = 0;
+
+// const guessMe = Math.floor(Math.random() * 120) + 19;
+// console.log(guessMe)
+const crys1 = Math.floor(Math.random() * 12) + 1;
+console.log(crys1);
+const crys2 = Math.floor(Math.random() * 12) + 1;
+console.log(crys2);
+const crys3 = Math.floor(Math.random() * 12) + 1;
+console.log(crys3);
+const crys4 = Math.floor(Math.random() * 12) + 1;
+console.log(crys4);
+
+// // Start Game Function
+
+// // Randomize number for player to guess (19-120)
 
 
 
-// Start Game Function
-
-// Randomize number for player to guess (19-120)
-
-const startGame = function() {
-
-guessValue = function getRandomIntInclusive(min, max) {
+const guessMe = function getRandomIntInclusive(min, max) {
         min = Math.ceil(19);
         max = Math.floor(120);
         return Math.floor(Math.random() * (max - min +1)) + min;
-        let matchvalue = guessValue();
-    };
 
+        
 
-crystalVals = function getRandomIntInclusive(min, max) {
-        min = Math.ceil(1);
-        max = Math.floor(12);
-        let crystalVals = [];
-        for (let i = 0; i < 4; i++); {
-        return Math.floor(Math.random() * (max - min +1)) + min;
-        crystalVals.push
-        }
-    };
-
-
-    console.log(guessValue());
-    console.log(crystalVals([0,1,2,3]));
-};
-
-
-startGame();
-$('.match-value').text(guessValue);
-
+}
+console.log(guessMe());
 })
+// $('.match-value').text(guessMe);
+
+// // crystalVals = function getRandomIntInclusive(min, max) {
+// //         min = Math.ceil(1);
+// //         max = Math.floor(12);
+// //         let crystalVals = [];
+// //         for (let i = 0; i < 4; i++); {
+// //             do { crystalVals = Math.floor(Math.random() * (max - min +1)) + min; } {
+// //         crystalVals.push
+// //         }
+// //         }
+// //     };
 
 
-// Assign value to guessValue
 
-
-// Randomize number for crystals on click (1-12)
-
-
-// Assign value to crystal values
+//     // console.log(crystalVals([0,1,2,3]));
 
 
 
+// startGame();
 
-// Increment value to Score
-
+// console.log(guessMe());
 // })
+
+
+// // Assign value to guessValue
+
+
+// // Randomize number for crystals on click (1-12)
+
+
+// // Assign value to crystal values
+
+
+
+
+// // Increment value to Score
+
+//})
